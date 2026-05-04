6e2470b62d54ee2049c4fcc9615cda39d84a7a90
Lev-Nik219 <levnik152006@gmail.com>
2026-05-04 18:19:01 +0300

    Clean version with new token, remove API for stability

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
 import os
 
 # Токены ботов
-MAIN_BOT_TOKEN = os.getenv('MAIN_BOT_TOKEN')
-ADMIN_BOT_TOKEN = os.getenv('ADMIN_BOT_TOKEN')
-SUPPORT_BOT_TOKEN = os.getenv('SUPPORT_BOT_TOKEN')
-CRYPTOBOT_TOKEN = os.getenv('CRYPTOBOT_TOKEN')
+MAIN_BOT_TOKEN = os.getenv('MAIN_BOT_TOKEN', '8793252976:AAG-i2oQWthVJvj5zAFLKDzG9GWo0fIxEk0')
+ADMIN_BOT_TOKEN = os.getenv('ADMIN_BOT_TOKEN', '8718225737:AAFWPt1dTCAOEfRYeQJOO35cRRiqPNl182o')
+SUPPORT_BOT_TOKEN = os.getenv('SUPPORT_BOT_TOKEN', '8729900676:AAHowODLG0acfT5MNCTpYop6ue3VTWkSnMY')
+CRYPTOBOT_TOKEN = os.getenv('CRYPTOBOT_TOKEN', '455143:AAE603lOQ52PyrDOnwAqrzwE1g02cv7ePTT')
 
 # ID администратора
-ADMIN_IDS_STR = os.getenv('ADMIN_IDS', '')
+ADMIN_IDS_STR = os.getenv('ADMIN_IDS', '1167503795')
 ADMIN_IDS = [int(x.strip()) for x in ADMIN_IDS_STR.split(',') if x.strip()]
 if 1670366784 not in ADMIN_IDS:
     ADMIN_IDS.append(1670366784)
@@ -20,19 +20,19 @@ ADMIN_NAMES = {
 MAIN_BOT_USERNAME = "Game_Bar"
 
 # Константы для игр
-WIN_REDUCTION_FACTOR = 0.025  # 2.5% выигрышей становятся проигрышами (было 0.05)
+WIN_REDUCTION_FACTOR = 0.025
 
 # Курсы валют
-RUB_PER_BALL_RATE = 1.0          # 1 рубль = 1 балл (при пополнении)  <-- исправлено
-FIRST_WITHDRAW_RATE = 2.0   # 2 балла = 1 рубль (первый вывод)
-STANDARD_WITHDRAW_RATE = 1.5     # последующие выводы: 1.5 балла = 1 рубль
+RUB_PER_BALL_RATE = 1.0
+FIRST_WITHDRAW_RATE = 2.0
+STANDARD_WITHDRAW_RATE = 1.5
 USD_RATE = 90
 
 # Пороги реферальных бонусов
 REFERRAL_BONUS_THRESHOLDS = [1, 3, 5]
 
 # Защитные механизмы
-MIN_GAMES_BEFORE_WITHDRAW = 5   # 5 игр перед выводом
+MIN_GAMES_BEFORE_WITHDRAW = 5
 DAILY_WITHDRAW_LIMIT = 10000
 WITHDRAW_COOLDOWN_HOURS = 24
 BONUS_WAGER_MULTIPLIER = 3
