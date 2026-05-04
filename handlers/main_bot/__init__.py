d258586a472696f87024803bde2edc72197f7acb
Lev-Nik219 <levnik152006@gmail.com>
2026-05-04 17:20:29 +0300

    Rename games.py and tournaments.py to _old versions

--- handlers/main_bot/__init__.py
+++ handlers/main_bot/__init__.py
@@ -1,6 +1,4 @@
-from .games import router as games_router
 from .profile import router as profile_router
-from .tournaments import router as tournaments_router
 from .payments import router as payments_router
 from .bot_info import router as bot_info_router
 from .fallback import router as fallback_router
